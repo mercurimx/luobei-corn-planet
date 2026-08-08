42fa7f6356c5de9a915b07e7e15f3b7d273223d9
mercurimx <mercurimovo@gmail.com>
2026-08-08 23:40:40 +0800

    Add GitHub Pages deployment

---
+++ vite.github.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+  publicDir: "public",
+  build: {
+    outDir: "dist-pages",
+    emptyOutDir: true,
+  },
+});
